65e0ddd6f300a1a6b6d28c8e7950def58d040cb3
Daniel Colman <danrcolman@gmail.com>
2021-12-02 15:27:59 -0500

    More Day 2

--- AdventofCode2021.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ AdventofCode2021.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -1,6 +1,59 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
-print(greeting)
+guard let fileURL = Bundle.main.url(forResource: "Day2Input", withExtension: "txt") else { fatalError() }
 
-Day1.printHi()
+var arrayOfStrings: [String] = []
+
+// Setup
+do {
+  let data = try String(contentsOf: fileURL, encoding: .utf8)
+  arrayOfStrings = data.components(separatedBy: "\n")
+} catch {
+  print(error)
+}
+
+// Part 1
+var down = 0
+var up = 0
+var forward = 0
+
+arrayOfStrings.forEach { string in
+  let tempStrArr = string.components(separatedBy: " ")
+  let num = Int(tempStrArr[1])!
+  switch tempStrArr[0] {
+  case "down":
+    down += num
+  case "up":
+    up += num
+  case "forward":
+    forward += num
+  default:
+    break
+  }
+}
+var finalVal = (down-up) * forward
+print(finalVal)
+
+// Part 2
+var aim = 0
+down = 0
+forward = 0
+
+arrayOfStrings.forEach { string in
+  let tempStrArr = string.components(separatedBy: " ")
+  let num = Int(tempStrArr[1])!
+  switch tempStrArr[0] {
+  case "down":
+    aim += num
+  case "up":
+    aim -= num
+  case "forward":
+    forward += num
+    down += num * aim
+  default:
+    break
+  }
+}
+
+finalVal = down * forward
+print(finalVal)
